5d31f2d4cbfb3423d927ed8cfb47bab900953222
Celuk <45749396+Celuk@users.noreply.github.com>
2021-12-14 13:11:31 +0300

    Update elf2encryptedhex.cpp

--- clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.cpp
+++ clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.cpp
@@ -1,5 +1,5 @@
-//===-- elf2encryptedhex.cpp -----------===//
-//
+//===---- elf2encryptedhex.cpp - elf2encryptedhex Hex Code Obfuscator -----===//
+//===---------------------- Ported From LLVM Tools ------------------------===//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
