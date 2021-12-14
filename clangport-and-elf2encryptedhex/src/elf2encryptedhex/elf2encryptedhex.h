c5e653abd463ad6326b257525a10d199f9cefa0d
Celuk <45749396+Celuk@users.noreply.github.com>
2021-12-14 13:09:12 +0300

    Update elf2encryptedhex.h

--- clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.h
+++ clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.h
@@ -1,3 +1,5 @@
+//===--------- elf2encryptedhex.h - elf2encryptedhex Header File ----------===//
+//===-------------------- Ported From llvm-objdump.h ----------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
