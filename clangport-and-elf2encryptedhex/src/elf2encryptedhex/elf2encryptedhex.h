0eacee91824b278edfb7aae47b5646a085c18f04
Celuk <45749396+Celuk@users.noreply.github.com>
2022-02-23 17:53:30 +0300

    Rebuttal update

--- clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.h
+++ clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.h
@@ -7,51 +7,39 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_TOOLS_LLVM_OBJDUMP_LLVM_OBJDUMP_H
-#define LLVM_TOOLS_LLVM_OBJDUMP_LLVM_OBJDUMP_H
+#ifndef LLVM_TOOLS_ELF2ENCHEX_H
+#define LLVM_TOOLS_ELF2ENCHEX_H
 
-#include "llvm/ADT/StringSet.h"
-#include "llvm/DebugInfo/DIContext.h"
-#include "llvm/MC/MCDisassembler/MCDisassembler.h"
-#include "llvm/Object/Archive.h"
+#include "llvm/Object/ObjectFile.h"
+
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Compiler.h"
-#include "llvm/Support/DataTypes.h"
+
+#include "llvm/ADT/StringRef.h"
 
 namespace llvm {
-class StringRef;
+class SymbolInfoTy;
 
 namespace object {
-class ELFObjectFileBase;
-class ELFSectionRef;
-class MachOObjectFile;
-class MachOUniversalBinary;
 class RelocationRef;
 } // namespace object
 
 namespace objdump {
 
-extern cl::opt<bool> ArchiveHeaders;
 extern cl::opt<bool> Demangle;
 extern cl::opt<bool> Disassemble;
 extern cl::opt<bool> DisassembleAll;
-extern cl::opt<DIDumpType> DwarfDumpType;
-extern cl::list<std::string> FilterSections;
+
+
 extern cl::list<std::string> MAttrs;
 extern cl::opt<std::string> MCPU;
-extern cl::opt<bool> NoShowRawInsn;
-extern cl::opt<bool> NoLeadingAddr;
-extern cl::opt<bool> PrintImmHex;
-extern cl::opt<bool> PrivateHeaders;
-extern cl::opt<bool> Relocations;
-extern cl::opt<bool> SectionHeaders;
-extern cl::opt<bool> SectionContents;
-extern cl::opt<bool> SymbolDescription;
-extern cl::opt<bool> SymbolTable;
-extern cl::opt<std::string> TripleName;
-extern cl::opt<bool> UnwindInfo;
 
-// All dots(.) replaced with underscore(_)
+extern cl::opt<bool> NoLeadingAddr;
+
+
+
+
+extern cl::opt<std::string> TripleName;
+
 
 // rv32i
 extern cl::opt<bool> lui;
@@ -641,9 +629,7 @@ extern cl::opt<std::string> cencq0insts; // 11 bit
 extern cl::opt<std::string> cencq1insts; // 21 bit
 extern cl::opt<std::string> cencq2insts; // 17 bit
 
-//////////////////////////////////////
 
-extern StringSet<> FoundSectionSet;
 
 typedef std::function<bool(llvm::object::SectionRef const &)> FilterPredicate;
 
@@ -709,21 +695,12 @@ private:
 SectionFilter ToolSectionFilter(llvm::object::ObjectFile const &O,
                                 uint64_t *Idx = nullptr);
 
-bool isRelocAddressLess(object::RelocationRef A, object::RelocationRef B);
-void printRelocations(const object::ObjectFile *O);
-void printDynamicRelocations(const object::ObjectFile *O);
-void printSectionHeaders(const object::ObjectFile *O);
-void printSectionContents(const object::ObjectFile *O);
-void printSymbolTable(const object::ObjectFile *O, StringRef ArchiveName,
-                      StringRef ArchitectureName = StringRef(),
-                      bool DumpDynamic = false);
-void printSymbol(const object::ObjectFile *O, const object::SymbolRef &Symbol,
-                 StringRef FileName, StringRef ArchiveName,
-                 StringRef ArchitectureName, bool DumpDynamic);
+
 LLVM_ATTRIBUTE_NORETURN void reportError(StringRef File, Twine Message);
 LLVM_ATTRIBUTE_NORETURN void reportError(Error E, StringRef FileName,
                                          StringRef ArchiveName = "",
                                          StringRef ArchitectureName = "");
+
 void reportWarning(Twine Message, StringRef File);
 
 template <typename T, typename... Ts>
@@ -733,8 +710,7 @@ T unwrapOrError(Expected<T> EO, Ts &&... Args) {
   reportError(EO.takeError(), std::forward<Ts>(Args)...);
 }
 
-std::string getFileNameForError(const object::Archive::Child &C,
-                                unsigned Index);
+
 SymbolInfoTy createSymbolInfo(const object::ObjectFile *Obj,
                               const object::SymbolRef &Symbol);
 
