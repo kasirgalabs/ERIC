d78ff928c85a1eca7c3bf45db5921fa5f7fdcc4a
Celuk <45749396+Celuk@users.noreply.github.com>
2022-02-24 12:36:57 +0300

    Rebuttal update

--- clangport-and-elf2encryptedhex/src/elf2encryptedhex/custom-encryptor.h
+++ clangport-and-elf2encryptedhex/src/elf2encryptedhex/custom-encryptor.h
@@ -1,5 +1,7 @@
+// To use custom_encrypt function to encrypt hexs set this flag as true
 bool custom = false;
 
+// Here data come as 32 or 16 bit string as binary, so you can customize the string with your desired encryption method
 std::string custom_encrypt(std::string bytes){
 	
 	return bytes;
