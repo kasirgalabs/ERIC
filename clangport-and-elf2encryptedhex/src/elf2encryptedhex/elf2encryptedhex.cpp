a00ddf85e0ceb540cda6e25b1e56d615d4ffe30d
Celuk <45749396+Celuk@users.noreply.github.com>
2022-02-24 09:34:58 +0300

    Rebuttal update

--- clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.cpp
+++ clangport-and-elf2encryptedhex/src/elf2encryptedhex/elf2encryptedhex.cpp
@@ -2454,6 +2454,7 @@ namespace portedDump {
   counter++;
 
   // Here we can add desired encryption method
+  // Also we can add desired encryption method into custom-encryptor.h and set custom flag to true
 
   if(custom == true){
     OSS << custom_encrypt(binArr);
